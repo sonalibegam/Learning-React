9578212b78fd056b54419b2dfce4836bedf3f824
23IUT0010107 <begamsonali586@gmail.com>
2025-09-20 16:57:03 +0530

    add files

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Hearder/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Hearder/LogoutBtn"
+import RTE from "./RTE";
+
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE
+}
